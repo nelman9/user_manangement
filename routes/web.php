ba9c7710cea21e5a4f81f3e33e479743307dbb26
nelman9 <nelsonombongi90@gmail.com>
2019-10-31 23:49:17 -0400

    login route

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,19 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+
 
 
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 Route::post('home.destroy/{id}', 'HomeController@destroy')->name('home.destroy');
 Route::delete('home.destroy/{id}', 'HomeController@destroy');
 Route::get('user.edit/{id}', 'HomeController@edit')->name('user.edit');
